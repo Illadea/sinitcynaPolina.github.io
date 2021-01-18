b2af3d7bb9da83652e57842b501d96de9d6858cc
Polina Sinitsyna <Pasi6@yandex.ru>
2021-01-18 16:46:53 +0300

    4 + 5 block, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('.tab-box__nav li:first').addClass('select');                // Первой вкладке добавляетсякласс select
+  $('.tab-box__nav li:first').addClass('select');                // Первой вкладке добавляется класс select
   $('.tab-box__panels>div').hide().filter(':first').show();      // Скрываются все блоки с описанием вкладок кроме первого
   $('.tab-box__nav a').click(function(){                         // При клике на вкладку
     $('.tab-box__panels>div').hide().filter(this.hash).show();   // - показывается блок с описанием текущей вкладки
