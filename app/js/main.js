eefbd26ff460c0e098e2a7d3a217212bc5a67da9
Polina Sinitsyna <Pasi6@yandex.ru>
2021-01-22 20:48:14 +0300

    mobile upd + nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+/* TABS */
 $(function(){
   $('.tab-box__nav li:first').addClass('select');                // Первой вкладке добавляется класс select
   $('.tab-box__panels>div').hide().filter(':first').show();      // Скрываются все блоки с описанием вкладок кроме первого
@@ -9,10 +10,27 @@ $(function(){
   })
 })
 
-
+/* NAVIGATION, ADDING BURGER MENU */
 $(document).ready(function() {
   $('.menu__hamburger-btn').click(function() {
       $('.menu__hamburger-btn').toggleClass('open-menu');
       $('.menu__list').toggleClass('open-menu');
+      $('body').toggleClass('fixed-page');
   });
 });
+
+
+
+/* NAVIGATION, when the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+    document.getElementById("header").style.boxShadow = "0 10px 30px -10px rgba(2,12,27,0.7)";
+  } 
+  else {
+    document.getElementById("header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
