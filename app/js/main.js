b57fc0df62cda854873e72a8391deed49bc245b2
Polina Sinitcyna <Pasi6@yandex.ru>
2021-03-28 23:14:01 +0300

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -1,58 +1,94 @@
-// /* TABS */
-$(function () {
-  $('.tab-box__nav li:first').addClass('tab-box__nav-item-selected');                // Первой вкладке добавляется класс tab-box__nav-item-selected
-  $('.tab-box__panels>div').hide().filter(':first').show();      // Скрываются все блоки с описанием вкладок кроме первого
-  $('.tab-box__nav a').click(function () {                         // При клике на вкладку
-    $('.tab-box__panels>div').hide().filter(this.hash).show();   // - показывается блок с описанием текущей вкладки
-    $('.tab-box__nav li').removeClass('tab-box__nav-item-selected');                 // - удаляется класс 'tab-box__nav-item-selected' у активной ранее вкладки
-    $(this).parent().addClass('tab-box__nav-item-selected');                    // - добавляется класс 'tab-box__nav-item-selected' для выбранной вкладки
-    return (false);                                         // - прерывается обработка события onClick
-  })
-})
+window.addEventListener('DOMContentLoaded', () => {
+
+  // TABS
+ const tabs = document.querySelectorAll('.tab-box__nav-item'),
+       tabsContent = document.querySelectorAll('.tab-box__content'),
+       tabsParent = document.querySelector('.tab-box__nav'); 
+
+ function hideTabContent() {
+   tabsContent.forEach(item => {
+     item.classList.add('hide');
+     item.classList.remove('show', 'fade');
+   });
+
+   tabs.forEach(item => {
+     item.classList.remove('tab-box__nav-item_active');
+   });
+
+ }
+
+ function showTabContent(i = 0) {
+   tabsContent[i].classList.add('show', 'fade');
+   tabsContent[i].classList.remove('hide');
+   tabs[i].classList.add('tab-box__nav-item_active');
+ }
+
+ hideTabContent();
+ showTabContent();
+ 
+ tabsParent.addEventListener('click', (e) => {
+   const target = e.target;
+
+   if(target && target.classList.contains('tab-box__nav-item')) {
+     tabs.forEach((item, i) => {
+       if (target == item) {
+         hideTabContent();
+         showTabContent(i);
+       } 
+     });
+   }
+ });
+ 
+
+  // NAVIGATION, BURGER MENU 
+  const menuHamburgerBtn = document.querySelector('.menu__hamburger-btn'),
+        menuList = document.querySelector('.menu__list'),
+        menuListItems = document.querySelectorAll('.menu__list-item'),
+        container = document.querySelector('.container');
 
 
+  function closeMenu() {
+    menuHamburgerBtn.classList.remove('open-menu');
+    menuList.classList.remove('open-menu');
+    document.body.classList.remove('fixed-page');
+  } 
 
-/* NAVIGATION, BURGER MENU */
-const menuHamburgerBtnElem = document.querySelector('.menu__hamburger-btn');
-const menuListElem = document.querySelector('.menu__list');
-const menuListItemElem = document.querySelector('.menu__list-item');
-const openMenuElem = document.querySelector('.open-menu');
-const fixedPageElem = document.querySelector('.fixed-page');
-const containerElem = document.querySelector('.container');
+  menuHamburgerBtn.addEventListener('click', () => {
+    menuHamburgerBtn.classList.toggle('open-menu');
+    menuList.classList.toggle('open-menu');
+    document.body.classList.toggle('fixed-page');
+  });
 
-menuHamburgerBtnElem.addEventListener('click', () => {
-  menuHamburgerBtnElem.classList.toggle('open-menu');
-  menuListElem.classList.toggle('open-menu');
-  document.body.classList.toggle('fixed-page');
+  menuListItems.forEach(item => {
+    item.addEventListener('click', () => {
+      closeMenu();
+    });
+  });
+
+  container.addEventListener('click', () => {
+    closeMenu();
+  });
+
+
+  // NAVIGATION: when user scrolls down, hide the navbar. When user scrolls up, show the navbar and add boxshadow to it
+  let prevScrollpos = window.pageYOffset;
+  window.addEventListener('scroll', () => {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("header").style.top = '0';
+      document.getElementById("header").style.boxShadow = '0 10px 30px -10px rgba(2,12,27,0.7)';
+    } else {
+      document.getElementById("header").style.top = '-111px';
+    }
+    if (window.pageYOffset === 0) {
+      document.getElementById("header").style.boxShadow = 'unset';
+      document.getElementById("header").style.top = '0px';
+    }
+    prevScrollpos = currentScrollPos;
+  });
 });
 
-menuListItemElem.addEventListener('click', () => {
-  menuHamburgerBtnElem.classList.remove('open-menu');
-  menuListElem.classList.remove('open-menu');
-  document.body.classList.remove('fixed-page');
-});
-
-containerElem.addEventListener('click', () => {
-  menuHamburgerBtnElem.classList.remove('open-menu');
-  menuListElem.classList.remove('open-menu');
-  document.body.classList.remove('fixed-page');
-});
 
 
 
-/* NAVIGATION: when user scrolls down, hide the navbar. When user scrolls up, show the navbar and add boxshadow to it*/
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = '0';
-    document.getElementById("header").style.boxShadow = '0 10px 30px -10px rgba(2,12,27,0.7)';
-  } else {
-    document.getElementById("header").style.top = '-111px';
-  }
-  if (window.pageYOffset === 0) {
-    document.getElementById("header").style.boxShadow = 'unset';
-    document.getElementById("header").style.top = '0px';
-  }
-  prevScrollpos = currentScrollPos;
-}
+
